0c88a710f4a51603ba371ec9ac7bf4f7f5820cc5
Uriel Robls <uriel_r@outlook.com>
2019-05-12 22:19:38 -0500

    render list

--- Cliet.rb
+++ Cliet.rb
@@ -6,7 +6,6 @@ class Nodo
     def initialize host_name, port_host
         @cliente_socket = TCPSocket.open host_name, port_host
         @data = []
-		@root = TkRoot.new
 		@self = self
         puts "Started node......."
 		run
@@ -15,7 +14,8 @@ class Nodo
     def run_client
         begin 
 			loop do
-				response = JSON.parse(@cliente_socket.gets)
+				#response = JSON.parse(@cliente_socket.gets)
+				response = @cliente_socket.gets
 				puts response
 				@data << response
 			end
@@ -34,36 +34,12 @@ class Nodo
 		@root.destroy()
 	end
 
-	def user_interface
-		myself = @self
-		content = Tk::Tile::Frame.new(@root) { padding "3 3 12 12" }
-		@names = @data.map {|item| "#{item.keys[0]}" }
-		@ips = @data.map {|item| "#{item.values[0]}" }
-		$names_list_variable = TkVariable.new @names
-		$ips_list_variable = TkVariable.new @ips
-		names_list = TkListbox.new(content) {listvariable $names_list_variable}
-		ips_list = TkListbox.new(content) {listvariable $ips_list_variable}
-		namelbl = Tk::Tile::Label.new(content) {text 'Nombre'}
-		iplbl = Tk::Tile::Label.new(content) {text 'Ip'}
-		close_conection = TkButton.new(content) { text "Cerrar conexion"; command(proc {myself.close_interface}) }
-		content.grid column: 0, row: 0, sticky: 'nsew'
-		namelbl.grid column: 1, row: 1, padx: 70
-		close_conection.grid column: 2, row: 3, pady: 10
-		names_list.grid column: 1, row: 2
-		ips_list.grid column: 2, row: 2
-		iplbl.grid column: 2, row: 1, padx: 70
-		Tk.mainloop
-	end
 
     def run
         client = Thread.new do 
             run_client
         end
-		gui = Thread.new do 
-			user_interface
-		end
 		client.join
-		gui.join
     end
 
 end
--- Server.rb
+++ Server.rb
@@ -1,4 +1,4 @@
-
+require 'tk'
 require 'socket'
 require 'json'
 
@@ -7,7 +7,7 @@ class Nodo
         @server_socket = TCPServer.open port_server
         @result = ' '
 		@connection_details = Array.new
-
+		@root = TkRoot.new
 		@pasconections = 0
 		@names = [:uriel, :andrea, :nacho, :carlos]
         puts "Started node......."
@@ -15,6 +15,23 @@ class Nodo
     end
 
     def run_server
+		myself = @self
+		content = Tk::Tile::Frame.new(@root) { padding "3 3 12 12" }
+		@names = @connection_details.map {|item| "#{item.keys[0]}" }
+		@ips = @connection_details.map {|item| "#{item.values[0]}" }
+		$names_list_variable = TkVariable.new @names
+		$ips_list_variable = TkVariable.new @ips
+		names_list = TkListbox.new(content) {listvariable $names_list_variable}
+		ips_list = TkListbox.new(content) {listvariable $ips_list_variable}
+		namelbl = Tk::Tile::Label.new(content) {text 'Nombre'}
+		iplbl = Tk::Tile::Label.new(content) {text 'Ip'}
+		close_conection = TkButton.new(content) { text "Cerrar conexion"; command(proc {myself.close_interface}) }
+		content.grid column: 0, row: 0, sticky: 'nsew'
+		namelbl.grid column: 1, row: 1, padx: 70
+		close_conection.grid column: 2, row: 3, pady: 10
+		names_list.grid column: 1, row: 2
+		ips_list.grid column: 2, row: 2
+		iplbl.grid column: 2, row: 1, padx: 70
 		index = -1
         begin
 			loop do 
@@ -24,8 +41,15 @@ class Nodo
 					sock_domain, remote_port, remote_hostname, remote_id = client.peeraddr
 					puts "connect to #{remote_id}"
 					#listen_client_conexions client, index, remote_id
-					@connection_details << Hash[@names[index] => remote_id].to_json
+					@connection_details << Hash[@names[index] => remote_id]
+
+					@names = @connection_details.map {|item| "#{item.keys[0]}" }
+					@ips = @connection_details.map {|item| "#{item.values[0]}" }
+					$names_list_variable.value = @names
+					$ips_list_variable.value = @ips
+
 					client.puts @connection_details 
+					puts @connection_details
 				end
 			end.join
         rescue => e
@@ -42,11 +66,19 @@ class Nodo
 		end
 	end
 
+	def user_interface
+		Tk.mainloop
+	end
+
     def run
         server = Thread.new do
             run_server
         end
+		gui = Thread.new do 
+			user_interface
+		end
         server.join
+		gui.join
     end
 end
 

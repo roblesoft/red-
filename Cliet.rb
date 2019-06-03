f839ee552fe989a6695a8764b3a5a978ce90c97c
Uriel Robles <uriel_r@outlook.com>
2019-06-02 19:16:29 -0500

    update}

--- Cliet.rb
+++ Cliet.rb
@@ -7,13 +7,35 @@ class Nodo
     def initialize host_name, port_host
         @cliente_socket = TCPSocket.open host_name, port_host
 		@cpu = 'AMD A9'
-		@memory = PosixPsutil::Memory.virtual_memory.total / 1e+9
-		@user = PosixPsutil::System.users[1].name
+		@memory = 100 - PosixPsutil::Memory.virtual_memory.percent
+		@user = "uriel"
 		@rank = 0
-		puts @memory
         puts "Started node......."
 		run_client
-    end
+	end
+	
+	def update_data
+		loop do 
+			@memory = 100 - PosixPsutil::Memory.virtual_memory.percent
+			if @cpu == 'intel i7'
+				@rank = 100
+			elsif @cpu == 'intel i5' || @cpu == 'AMD A9'
+				@rank = 80
+			elsif @cpu == 'intel i3' 
+				@rank = 50
+			end
+			if @memory >= 80.0
+				@rank += 100
+			elsif @memory >= 70
+				@rank += 90
+			elsif @memory >= 50
+				@rank += 30
+			end
+			information = Hash[name: @user, memory: @memory, cpu: @cpu, rank: @rank].to_json
+			@cliente_socket.puts information
+			sleep 1000
+		end
+	end
 
     def run_client
         begin 
@@ -25,13 +47,14 @@ class Nodo
 				elsif @cpu == 'intel i3' 
 					@rank = 50
 				end
-				if @memory >= 12.0
+				if @memory >= 80.0
 					@rank += 100
-				elsif @memory >= 8.0
+				elsif @memory >= 70
 					@rank += 90
-				elsif @memory >= 4.0
+				elsif @memory >= 50
 					@rank += 30
 				end
+
 				information = Hash[name: @user, memory: @memory, cpu: @cpu, rank: @rank].to_json
 				@cliente_socket.puts information
 				if gets.to_s.chomp == 'q'
--- Server.rb
+++ Server.rb
@@ -6,7 +6,7 @@ class Nodo
     def initialize port_server
         @server_socket = TCPServer.open port_server
         @result = ' '
-		@connection_details = Array.new
+		@connection_details = Hash.new
 		@connection_info = Hash.new
 		@connected_clients = Hash.new
 		@connection_info[:server] = @server_socket
@@ -17,6 +17,7 @@ class Nodo
 		@ips = []
 		@cpus = []
 		@ranks = []
+		@index = 0
         puts "Started node......."
         run
     end
@@ -37,18 +38,37 @@ class Nodo
 		$cpus_list_variable = TkVariable.new @cpus
 		$ranks_list_variable = TkVariable.new @rank
 
-		names_list = TkListbox.new(content) {listvariable $names_list_variable}
-		ips_list = TkListbox.new(content) {listvariable $ips_list_variable}
-		ram_list = TkListbox.new(content) {listvariable $rams_list_variable}
-		cpu_list = TkListbox.new(content) {listvariable $cpus_list_variable}
-		rank_list = TkListbox.new(content) {listvariable $ranks_list_variable}
+		names_list = TkListbox.new(content) do 
+			listvariable $names_list_variable 
+			width 10 
+		end
+		ips_list = TkListbox.new(content) do 
+			listvariable $ips_list_variable 
+			width 10 
+		end
+		ram_list = TkListbox.new(content)  do 
+			listvariable $rams_list_variable 
+			width 10 
+		end
+		cpu_list = TkListbox.new(content)  do
+			listvariable $cpus_list_variable
+			width 10 
+		end
+		rank_list = TkListbox.new(content)  do 
+			listvariable $ranks_list_variable 
+			width 10 
+		end
 
 		namelbl = Tk::Tile::Label.new(content) {text 'Nombre'}
 		iplbl = Tk::Tile::Label.new(content) {text 'Ip'}
 		ramlbl = Tk::Tile::Label.new(content) {text 'Ram'}
 		cpulbl = Tk::Tile::Label.new(content) {text 'Cpu'}
 		ranklbl = Tk::Tile::Label.new(content) {text 'Ranking'}
-		close_conection = TkButton.new(content) { text "Cerrar conexion"; command(proc {myself.close_interface}) }
+		close_conection = TkButton.new(content) do 
+			text "Cerrar"
+			command(proc {myself.close_interface}) 
+			width 10
+		end
 
 		content.grid column: 0, row: 0, sticky: 'nsew'
 		namelbl.grid column: 1, row: 1
@@ -63,7 +83,7 @@ class Nodo
 		rank_list.grid column: 5, row: 2
 		iplbl.grid column: 2, row: 1
 
-		def update_data name, ips, cpus, rams, ranks
+		def create_data name, ips, cpus, rams, ranks
 
 			$names_list_variable.value = name
 			$ips_list_variable.value = ips
@@ -73,37 +93,55 @@ class Nodo
 		end
 
 		
+		def update_data index
+			loop do 
+				sleep 1000
+				response = JSON.parse(client.gets)
+				puts response
+				@connection_details[index] = response
+
+				@names = @connection_details.map {|item, value| value['name'] }
+				@ips = @connection_details.map {|item, value| remote_id }
+				@rams = @connection_details.map {|item, value| "#{value["memory"]} %"} 
+				@cpus = @connection_details.map {|item, value| value['cpu']}
+				@ranks = @connection_details.map {|item, value| value['rank']}
+
+				create_data @names, @ips, @cpus, @rams, @ranks
+			end
+		end
+		
         begin
 			loop do 
 				@client_connection = @server_socket.accept
-				Thread.start(@client_connection) do |client|
+				@threads = Thread.start(@client_connection) do |client|
 					sock_domain, remote_port, remote_hostname, remote_id = client.peeraddr
 					puts "connect to #{remote_id}"
+					@index += 1
+					client_number = @index
 					response = JSON.parse(client.gets)
 					puts response
-					@connection_details << response
+					@connection_details[@index] = response
 
-					@names = @connection_details.map {|item| item['name'] }
-					@ips = @connection_details.map {|item| remote_id }
-					@rams = @connection_details.map {|item| "#{item["memory"]} Gb"} 
-					@cpus = @connection_details.map {|item| item['cpu']}
-					@ranks = @connection_details.map {|item| item['rank']}
+					@names = @connection_details.map {|item, value| value['name'] }
+					@ips = @connection_details.map {|item, value| remote_id }
+					@rams = @connection_details.map {|item, value| "#{value["memory"]} %"} 
+					@cpus = @connection_details.map {|item, value| value['cpu']}
+					@ranks = @connection_details.map {|item, value| value['rank']}
 
-					update_data @names, @ips, @cpus, @rams, @ranks
+					create_data @names, @ips, @cpus, @rams, @ranks
+					update_data client_number
 
 				end
-			end.join
+			end
         rescue => e
             puts e.message
-            @client_connection.close
-        ensure
             @client_connection.close
         end
     end
 
 	def close_interface
 		puts "close conexion"
-		@cliente_connection.close
+		@server.kill
 		@root.destroy()
 	end
 
@@ -112,13 +150,13 @@ class Nodo
 	end
 
     def run
-        server = Thread.new do
+        @server = Thread.new do
             run_server
         end
 		gui = Thread.new do 
 			user_interface
 		end
-        server.join
+        @server.join
 		gui.join
     end
 end
